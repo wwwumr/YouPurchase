2cff1159e2a6c32ab5ea19b21edef66fa7f1f622
wwwumr <1127858301@qq.com>
2019-07-02 22:13:17 +0800

    add shop management

---
+++ code/manager/src/mock/shopMock.js
@@ -0,0 +1,19 @@
+var shopMock = [
+    {
+        key: 0, 
+        storeName: "华山派", 
+        address: "河南", 
+        coverPicUrl: '/img/1.png', 
+        contact: '123457', 
+        hours: [ "8:00", '21:30' ]
+    },{
+        key: 1, 
+        storeName: "武当派", 
+        address: "河北", 
+        coverPicUrl: '/img/2.png', 
+        contact: '1234457', 
+        hours: [ "8:30", '21:00' ]
+    }
+]
+
+export default shopMock;
