39014455627f7b7beefd5aa00a3dfebfcc1b8b0e
softwaredengxiao <3502685232@qq.com>
2019-07-02 10:02:16 +0800

    orderinfo类新增status状态，用户相关实体类

--- code/backend/src/main/java/com/sjtu/youpurchase/entity/User.java
+++ code/backend/src/main/java/com/sjtu/youpurchase/entity/User.java
@@ -17,8 +17,24 @@ public class User {
     @Column(name = "userId")
     private Long Id;
 
+    @Column(nullable = false,unique = true,length=30)
     private String userName;
 
+    @Column(nullable = false,length=30)
     private String password;
+
+    //地区位置
+    @Column(length=30)
+    private String area;
+
+    //街道地址
+    @Column(length=30,nullable = false)
+    private String address;
+
+    @Column(length=11)
+    private String phone;
+
+    @Column(length=256)
+    private String imgFile;
 
 }
