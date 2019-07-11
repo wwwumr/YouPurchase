319a3e86480fb0e23ad88b19e6bd06c8bde42a4e
softwaredengxiao <3502685232@qq.com>
2019-07-11 09:11:55 +0800

    modified version

---
+++ code/backend-user/src/main/java/com/you_purchase/backenduser/BackenduserApplication.java
@@ -0,0 +1,13 @@
+package com.you_purchase.backenduser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackenduserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackenduserApplication.class, args);
+	}
+
+}
