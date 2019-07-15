4bbac1192ee5d01d590bc8131ce585017040d337
chuyuxuan123 <chuyuxuan@126.com>
2019-07-15 17:19:17 +0800

    添加距离计算的工具类

---
+++ code/adminanddealer-backend/src/test/java/com/sjtu/adminanddealer/utils/DistanceUtilTest.java
@@ -0,0 +1,28 @@
+package com.sjtu.adminanddealer.utils;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.junit.Assert.*;
+
+/**
+ * 计算距离测试
+ *
+ * @author Chuyuxuan
+ */
+public class DistanceUtilTest {
+
+    private DistanceUtil distanceUtil = new DistanceUtil();
+
+    /**
+     * 计算交大与华师的距离，结果为2.1km，与实际相符
+     */
+    @Test
+    public void getDistance() {
+        System.out.println(distanceUtil.getDistance(121.4374,31.022739,
+                121.457141,31.028182));
+        Assert.assertTrue(distanceUtil.getDistance(121.4374,31.022739,
+                121.457141,31.028182)<3);
+    }
+}
