31aa607496afc590c4c6f2c1448254556ba3fd59
wwwumr <1127858301@qq.com>
2019-07-17 09:24:27 +0800

    增加test

--- code/manager/src/test/HomePage.test.js
+++ code/manager/src/test/HomePage.test.js
@@ -28,13 +28,27 @@ describe('测试HomePage组件', () => {
         let homePageShallow = shallow(
             <Homepage changeBg={changeBg} fn={fn} logIn={false} />
         );
-        // componentDidMount时使用changeBg
+        // componentDidMount时使用changeBg而fn未被使用
         expect(changeBg).toHaveBeenCalledTimes(1);
         expect(fn).not.toHaveBeenCalled();
         expect(homePageShallow.props().logIn).toBeFalsy();
     })
 
     test('测试子组件构成', () => {
+        let changeBg = jest.fn();
+        let fn = jest.fn(x => x);
+
+        let homePageMount = mount(
+            <Homepage changeBg={changeBg} fn={fn} logIn={false} />
+        );
+        
+        // 测试子组件
+        expect(homePageMount.find('Title').length).toBe(1);
+        expect(homePageMount.find('Title').props().children).toEqual(" 欢迎登录优邻购管理系统 ");
+        expect(homePageMount.find('LogInForm').exists).toBeTruthy();
+    });
+
+    test('测试父组件方法', () => {
         let changeBg = jest.fn();
         let fn = jest.fn(x => x);
 
@@ -48,12 +62,7 @@ describe('测试HomePage组件', () => {
         // 测试fn
         homePageMount.props().fn("步惊云");
         expect(fn.mock.calls[0][0]).toMatch("步惊云");
-        
-        expect(homePageMount.find('Title').length).toBe(1);
-        expect(homePageMount.find('Title').props().children).toEqual(" 欢迎登录优邻购管理系统 ");
-        expect(homePageMount.find('LogInForm').exists).toBeTruthy();
     });
-
 
 });
 
