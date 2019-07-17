373634da4e9e230b9bf16fa45608c266e1983258
chuyuxuan123 <chuyuxuan@126.com>
2019-07-17 13:49:03 +0800

    添加遗漏文件

---
+++ code/delivery-mock/src/main/java/com/sjtu/deliverymock/DeliveryMockApplication.java
@@ -0,0 +1,13 @@
+package com.sjtu.deliverymock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryMockApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeliveryMockApplication.class, args);
+    }
+
+}
