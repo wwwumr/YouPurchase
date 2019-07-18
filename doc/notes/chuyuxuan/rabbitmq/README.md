a3f57bfe27c71b065bda7058780510a84c347c7d
chuyuxuan123 <chuyuxuan@126.com>
2019-07-18 21:35:44 +0800

    rabbitmq学习

---
+++ doc/notes/chuyuxuan/rabbitmq/README.md
@@ -0,0 +1,21 @@
+# RabbitMQ学习
+
+## RabbitMQ安装
+直接官网找安装包，下载后提示安装Erlang，按照提示next就行
+
+参考：
+
+* https://baijiahao.baidu.com/s?id=1608425504960724381&wfr=spider&for=pc
+
+## 具体用例
+看了几篇文章，把能跑出来参考的贴一下
+* 官网上的 https://spring.io/guides/gs/messaging-rabbitmq/
+* CSDN SpringBoot + RabbitMQ 简单实现创建队列的三种方式 https://blog.csdn.net/antma/article/details/84311601
+* SpringBoot整合RabbitMQ https://blog.csdn.net/u013871100/article/details/82982235
+
+## 继续学习
+关于消息队列的几种模式还没有看，可以去官网上有详细的guide
+
+## references
+* https://docs.spring.io/spring-amqp/docs/2.1.7.RELEASE/reference/html/
+* https://www.rabbitmq.com/getstarted.html
---
+++ doc/notes/chuyuxuan/rabbitmq/src/test/java/com/example/rabbittest/SenderTest.java
@@ -0,0 +1,24 @@
+package com.example.rabbittest;
+
+import org.junit.Test;
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Date;
+
+public class SenderTest extends RabbitTestApplicationTests {
+
+    @Autowired
+    private AmqpTemplate amqpTemplate;
+
+    @Test
+    public void send2(){
+        amqpTemplate.convertAndSend("myQueue2", "2 now " + new Date());
+    }
+
+    @Test
+    public void send3(){
+        amqpTemplate.convertAndSend("myQueue3", "3 now " + new Date());
+    }
+}
