d350d00ce102d906d7a5c2c244b36f6fe0aa1d53
chuyuxuan123 <chuyuxuan@126.com>
2019-07-23 15:19:58 +0800

    移出无用图片;添加Prometheus学习记录;

--- code/adminanddealer-backend/src/main/java/com/sjtu/adminanddealer/controller/AdminDealerLoginController.java
+++ code/adminanddealer-backend/src/main/java/com/sjtu/adminanddealer/controller/AdminDealerLoginController.java
@@ -7,8 +7,6 @@ import com.sjtu.adminanddealer.entity.Dealer;
 import com.sjtu.adminanddealer.entity.User;
 import com.sjtu.adminanddealer.parameter.UserLoginParameter;
 import com.sjtu.adminanddealer.service.AdminDealerLoginService;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -19,14 +17,12 @@ import javax.servlet.http.HttpSession;
  */
 @CrossOrigin
 @RestController
-@Api(tags = "管理员、经销商、用户登录相关")
 public class AdminDealerLoginController {
 
     @Autowired
     private AdminDealerLoginService adminDealerLoginService;
 
     @GetMapping("/login/admin")
-    @ApiOperation(value = "管理员登录")
     public String adminLogin(@RequestParam("userName") String userName, @RequestParam("password") String password,
                              HttpSession session) {
         Admin admin = adminDealerLoginService.getAdminByUserNameAndPassword(userName, password);
