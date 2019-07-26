781eeec38c1f7321dee0b7fc229509f81b088e33
softwaredengxiao <3502685232@qq.com>
2019-07-26 16:39:04 +0800

    新增第四周工作报告，新增学习日志，尝试添加RabbitMQ

--- code/backend-user/src/main/java/com/you_purchase/backenduser/BackenduserApplication.java
+++ code/backend-user/src/main/java/com/you_purchase/backenduser/BackenduserApplication.java
@@ -1,6 +1,6 @@
 package com.you_purchase.backenduser;
 
-import org.springframework.amqp.rabbit.annotation.EnableRabbit;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
