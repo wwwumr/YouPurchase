e12a567dfb58b6d1c0f59277ebd7d6521d23281a
chuyuxuan123 <chuyuxuan@126.com>
2019-07-26 11:21:57 +0800

    websocket学习

---
+++ code/adminanddealer-backend/src/test/java/com/sjtu/adminanddealer/dao/UserDaoTest.java
@@ -0,0 +1,46 @@
+package com.sjtu.adminanddealer.dao;
+
+import com.sjtu.adminanddealer.entity.User;
+import com.sjtu.adminanddealer.repository.UserRepository;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.Assert.*;
+
+/**
+ * UserDao的测试类.
+ *
+ * @author Chuyuxuan
+ */
+@Transactional
+@RunWith(SpringRunner.class)
+@ActiveProfiles("test")
+@SpringBootTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+public class UserDaoTest {
+    @Autowired
+    private UserDao userDao;
+
+    @Test
+    public void testGetUserByUserNameAndPassword() throws Exception {
+        User user = userDao.getUserByUserNameAndPassword("test", "1234");
+        Assert.assertNotNull(user);
+        User user1 = userDao.getUserByUserNameAndPassword("wrong", "123");
+        Assert.assertNull(user1);
+    }
+
+    @Test
+    public void testGetUserByPhoneAndPassword() throws Exception {
+        User user = userDao.getUserByPhoneAndPassword("1234567", "1234");
+        Assert.assertNotNull(user);
+        User user1 = userDao.getUserByPhoneAndPassword("020-1234", "abcd");
+        Assert.assertNull(user1);
+    }
+}
---
+++ doc/notes/chuyuxuan/websocket/README.md
@@ -0,0 +1,5 @@
+# WebSocket
+## 使用websocket给前端推送消息
+* 添加websocket依赖
+* 添加配置类
+* 添加websocket类@ServerEndpoint，override各种方法
