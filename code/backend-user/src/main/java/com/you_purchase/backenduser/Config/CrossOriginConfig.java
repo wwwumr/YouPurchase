dd35ffad422a369c06a74bb20b852e402914b9d6
wwwumr <1127858301@qq.com>
2019-07-31 10:51:31 +0800

    订单界面优化及功能添加

--- code/backend-user/src/main/java/com/you_purchase/backenduser/Config/CrossOriginConfig.java
+++ code/backend-user/src/main/java/com/you_purchase/backenduser/Config/CrossOriginConfig.java
@@ -1,18 +1,16 @@
-/*
+
 package com.you_purchase.backenduser.Config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.*;
 
-*/
+
 /**
  * 使用session拦截请求，未登录的用户无法访问到接口
  * 增加跨域配置，便于前后端调试
  *
  * @author Chuyuxuan
- *//*
+ */
 
 @Configuration
 @EnableWebMvc
@@ -31,6 +29,19 @@ public class CrossOriginConfig implements WebMvcConfigurer {
                 //跨域允许时间
                 .maxAge(3600);
     }
-
+    
+    /**
+     * swagger-ui配置
+     *
+     * @param registry
+     */
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/statics/**").addResourceLocations("classpath:/statics/");
+        registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+        registry.addResourceHandler("/swagger/**").addResourceLocations("classpath:/statics/swagger/");
+    }
 }
-*/
+
+
