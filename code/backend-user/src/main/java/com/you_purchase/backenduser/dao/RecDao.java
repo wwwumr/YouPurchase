84771333b3da521a77c7ea72070b7756f142e214
softwaredengxiao <3502685232@qq.com>
2019-09-07 14:11:29 +0800

    回退版本

--- code/backend-user/src/main/java/com/you_purchase/backenduser/dao/RecDao.java
+++
@@ -1,8 +0,0 @@
-package com.you_purchase.backenduser.dao;
-
-import com.you_purchase.backenduser.entity.Recommend;
-import org.springframework.data.repository.CrudRepository;
-
-public interface RecDao extends CrudRepository<Recommend,String > {
-    public Recommend findByRecId(long recId);
-}
