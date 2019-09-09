4f0071d2d5042fcabff74d435b118feffde11277
wwwumr <1127858301@qq.com>
2019-09-09 16:29:48 +0800

    更改config以适应远端接口

--- code/manager/src/config/config.js
+++ code/manager/src/config/config.js
@@ -1,5 +1,5 @@
 let root = "http://202.120.40.8:30414/";
-let root1 = "http://202.120.40.8:30413";
+let root1 = "http://202.120.40.8:30413/";
 /*
 var root = "http://localhost:9000/";
 var root1 = "http://localhost:9001/";
